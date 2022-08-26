d567acfe3b1404902834a4a7de28ee309eade6d7
pec05 <leandro-peccio@hotmail.com>
2022-08-26 10:48:31 +0200

    Initial commit

---
+++ src/main/java/com/example/carnetmusculationapi/CarnetMusculationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carnetmusculationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetMusculationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarnetMusculationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carnetmusculationapi/CarnetMusculationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carnetmusculationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetMusculationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
